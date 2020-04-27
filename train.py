7cc641c22a71e8725291deb1338a024e00b04355
hirune924 <nori.o920924@outlook.jp>
2020-04-27 12:12:00 +0900

    tmp

--- train.py
+++ train.py
@@ -257,7 +257,6 @@ def main(hparams):
     tb_logger = loggers.TensorBoardLogger(save_dir=hparams.log_dir, name='default', version=None)
  
     logger_list = [tb_logger, neptune_logger] if hparams.distributed_backend!='ddp' else tb_logger
-    print(logger_list)
 
     checkpoint_callback = ModelCheckpoint(
         filepath=os.path.join(hparams.log_dir, '{epoch}-{avg_val_loss}-{val_qwk}'),
