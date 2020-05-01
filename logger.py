becc948e5b7df062c728bbf9365565177da9f1d6
hirune924 <nori.o920924@outlook.jp>
2020-05-01 11:33:07 +0900

    bug fix

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from argparse import Namespace
 from typing import Optional, List, Dict, Any, Union, Iterable
 from pytorch_lightning.logging.neptune import NeptuneLogger
 try:
